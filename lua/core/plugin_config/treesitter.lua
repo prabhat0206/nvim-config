884a6f76ef2cf5e2315a2f2c9df5398013a4e2e4
Prabhat Ranjan <63228333+prabhat0206@users.noreply.github.com>
2023-12-09 14:36:38 +0530

    updatee

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "rust", "vim", "json", "yaml", "toml", "html", "css", "javascript", "typescript", "tsx", "python", "bash", "go", "java", "php", "ruby", "vue", "svelte", "dart", "dockerfile", "graphql", "haskell", "jsonc", "latex", "ocaml", "ocaml_interface", "ocamllex", "purescript", "regex", "scala", "scss", "sparql", "supercollider", "swift", "teal", "turtle", "verilog", "yaml", "zig" },
+  ensure_installed = { "c", "lua", "rust", "vim", "json", "yaml", "toml", "html", "css", "javascript", "typescript", "tsx", "python", "bash", "go", "java", "php", "ruby", "vue", "svelte", "dart", "dockerfile", "graphql", "haskell", "jsonc", "latex", "ocaml", "ocaml_interface", "purescript", "regex", "scala", "scss", "sparql", "supercollider", "turtle", "verilog", "yaml", "zig" },
   sync_install = false,
   auto_install = true,
   highlight = {
